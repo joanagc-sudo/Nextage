2ec931d3a90ff90d8ce1e3c0e1f56bf89cd8fbf9
joanagc-sudo <joanagc.goncalves@gmail.com>
2026-08-23 18:40:17 -0300

    vnode.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "indexCadastroAluno.html"));
+});
+
+http.createServer(app).listen(8080, () => {
+    console.log("Servidor rodando na porta 8080");
+});
