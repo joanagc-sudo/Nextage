bbb9a49b431be4a2efbeb8df85e0e52f6fd7ec6a
joanagc-sudo <you@example.com>
2026-08-25 09:04:50 -0300

    hbs

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "indexTelaInicial.html"));
 });
 
-app.get("/telainicial", function(req, res) {
+app.get("/tela-inicial", function(req, res) {
     res.render("indexTelaInicial");
 });
 
-app.get("/indexMateriais", function(req, res) {
+app.get("/materiais", function(req, res) {
     res.render("indexMateriais");
+});
+
+app.get("/login", function(req, res) {
+    res.render("indexTelaLogin");
 });
 
 http.createServer(app).listen(8080, () => {
