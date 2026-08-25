9f9769d340fd4a8b72e86742e1c5693aec74cffd
juliagkehl <juliagkehl@gmail.com>
2026-08-25 08:34:19 -0300

    Adicionando o hbs e nodemon no projeto

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const path = require("path");
 
 const app = express();
 
+app.set('view engine', 'hbs');
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "indexCadastroAluno.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "indexTelaInicial.html"));
+});
+
+app.get("/telainicial", function(req, res) {
+    res.render("indexTelaInicial");
 });
 
 http.createServer(app).listen(8080, () => {
